3e8d96a69f21dbc901a1e7720d0b6275c29cbce6
Mezzeki <bloodi7on31@gmail.com>
2026-07-25 20:11:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const config = require('./config/config');
+const { processOrders } = require('./services/orders');
+
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('OK');
 });
 
-app.listen(port, () => {
-  console.log('Server running on port ' + port);
+app.post('/webhook', (req, res) => {
+  console.log('Webhook received:', req.body);
+  res.send('OK');
+});
+
+// Запускаем проверку заказов сразу при старте
+processOrders();
+
+// И каждые 30 секунд
+setInterval(processOrders, config.checkInterval);
+
+app.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
 });
